1680f5d8ea16a14ea11ac7d5936a7959e8121fa8
Pradheep P <mantissa6789@gmail.com>
2024-10-27 00:27:45 +0530

    starting with it

--- server/server.js
+++ server/server.js
@@ -18,7 +18,35 @@ mongoose
     .then(() => console.log("MongoDB connected..."))
     .catch(() => console.log("MongoDB connection failed"));
 
-//user : name, email, password(hashed), 
+//user : user_id, name, email, password(hashed), leader, member, group_name, group_id
+//group : group_id, name, leader, members, created_at, updated_at, deleted_at, group_password
+
+const userSchema = new mongoose.Schema({
+    user_id : {type : String, required : true, unique : true, index : true},
+    name : {type : String, required : true},
+    email : {type : String, required : true, unique : true},
+    password : {type : String, required : true},
+    leader : {type : Boolean, required : true, default : false},
+    member : {type : Boolean, required : true, default : true},
+    group_name : {type : String, required : true},
+    group_id : {type : String, required : true, unique : true, index : true},
+}, 
+{timestamps : true},
+);
+
+const groupSchema = new mongoose.Schema({
+    group_id : {type : String, required : true, unique : true, index : true},
+    name : {type : String, required : true},
+    leader : {type : String, required : true},
+    members : [{type: mongoose.Schema.Types.ObjectId, ref : 'User'}],
+    created_at : {type : Date, required : true, default : Date.now()},
+    updated_at : {type : Date, required : true, default : Date.now()},
+    group_password : {type : String, required : true},
+});
+
+const User = mongoose.model("User", userSchema);
+const Group = mongoose.model("Group", groupSchema);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
